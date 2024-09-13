bccf37c2bff599ede901a444914b870d9d5f2086
simz1996 <simairamou8@gmail.com>
2024-09-14 07:13:43 +0800

    made more changes on full-image-page

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,19 +1,31 @@
-
-
+import { clerkClient } from "@clerk/nextjs/server";
 import { getImage } from "~/server/quaries";
 
 
 export default async function FullPageImageView(props: {id: number}){
 
   const image = await getImage(props.id);
+
+  const uploaderInfo = await clerkClient.users.getUser(image.userId);
   return (
     <div className="flex h-full w-full min-w-0 bg-black">
       <div className="flex-shrink flex justify-center items-center">
       <img src={image.url} className="object-contain flex shrink" alt={image.name} />
       </div>
 
-      <div className="w-45 flex flex-col flex-shrink-0 border-l zinc-900 h-full">
+      <div className="w-48 flex flex-col flex-shrink-0 border-l zinc-900 h-full ">
         <div className="text-xl font-bold border-b text-center">{image.name}</div>
+
+        <div className="flex flex-col px-2">
+          <span>Uploaded By:</span>
+          <span>{uploaderInfo.username}</span>
+        </div>
+
+        {/* Add any additional information here */}
+        <div className="flex flex-col px-2 mt-4">
+          <span>Created On:</span>
+          <span>{new Date(image.createdAt).toLocaleDateString()}</span>
+        </div>
       </div>
 
     </div>
