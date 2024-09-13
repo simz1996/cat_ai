755430e091de08c1f8d8f4bafb3fbbd1e601bd95
simz1996 <simairamou8@gmail.com>
2024-09-14 04:52:57 +0800

    made changes to the full-image-page file

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,3 +1,5 @@
+
+
 import { getImage } from "~/server/quaries";
 
 
@@ -5,12 +7,12 @@ export default async function FullPageImageView(props: {id: number}){
 
   const image = await getImage(props.id);
   return (
-    <div className="flex h-full w-full min-w-0">
+    <div className="flex h-full w-full min-w-0 bg-black">
       <div className="flex-shrink flex justify-center items-center">
       <img src={image.url} className="object-contain flex shrink" alt={image.name} />
       </div>
 
-      <div className="w-48 flex flex-col flex-shrink-0 border-l">
+      <div className="w-45 flex flex-col flex-shrink-0 border-l zinc-900 p-4 ">
         <div className="text-xl font-bold">{image.name}</div>
       </div>
 
